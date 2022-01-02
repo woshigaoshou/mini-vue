0bfabe81c1800e514ac27d3fd3beaae8adcbc45a
woshigaoshou <1258006989@qq.com>
2022-01-03 03:14:45 +0800

    增加Proxy的数据劫持方式

--- reactive.js
+++ reactive.js
@@ -35,22 +35,39 @@ function getDep(target, key) {
   return dep;
 }
 
+// vue2实现方式
+// const reactive = (raw) => {
+//   for (let key in raw) {
+//     const dep = getDep(raw, key);
+//     let value = raw[key]; // 若不定义value，则会不断触发get，导致栈溢出
+//     Object.defineProperty(raw, key, {
+//       get() {
+//         dep.depend();
+//         return value;
+//       },
+//       set(newValue) {
+//         value = newValue;
+//         dep.notify();
+//       }
+//     })
+//   }
+//   return raw;
+// }
+
+// vue3实现方式
 const reactive = (raw) => {
-  for (let key in raw) {
-    const dep = getDep(raw, key);
-    let value = raw[key]; // 若不定义value，则会不断触发get，导致栈溢出
-    Object.defineProperty(raw, key, {
-      get() {
-        dep.depend();
-        return value;
-      },
-      set(newValue) {
-        value = newValue;
-        dep.notify();
-      }
-    })
-  }
-  return raw;
+  return new Proxy(raw, {
+    get(target, key) {
+      const dep = getDep(target, key);
+      dep.depend();
+      return target[key];
+    },
+    set(target, key, newValue) {
+      const dep = getDep(target, key);
+      target[key] = newValue;
+      dep.notify();
+    }
+  });
 }
 
 function watchEffect(effect) {
